a2475866b855a9f8d4b7fc2ea946394775482f5e
angellee177 <angelriapurnamasari17@gmail.com>
2025-12-31 19:42:35 +0800

    feat: add scene design

--- Assets/Scripts/CrashDetectorHandler.cs
+++ Assets/Scripts/CrashDetectorHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class CrashDetectorHandler : MonoBehaviour
 {
     [SerializeField] float delayTime;
+    [SerializeField] ParticleSystem crashParticle;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerIndex = LayerMask.NameToLayer("Floor");
@@ -12,6 +13,7 @@ public class CrashDetectorHandler : MonoBehaviour
 
         if(collision.gameObject.layer == layerIndex)
         {
+            crashParticle.Play();
             Invoke("ReloadScene", delayTime);
         }
     }
--- Assets/Scripts/FinishLineHandler.cs
+++ Assets/Scripts/FinishLineHandler.cs
@@ -4,12 +4,14 @@ using UnityEngine.SceneManagement;
 public class FinishLineHandler : MonoBehaviour
 {
     [SerializeField] float delayTime;
+    [SerializeField] ParticleSystem finishParticles;
     void OnTriggerEnter2D(Collider2D collision)
     {
         int layerindex = LayerMask.NameToLayer("Player");
 
         if(collision.gameObject.layer == layerindex)
         {
+            finishParticles.Play();
             Invoke("ReloadScene", delayTime);
         }
     }
